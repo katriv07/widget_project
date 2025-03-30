ec4ef319ea0b3a219add7c30dca38cb4428f9b5c
Югова Екатерина <yugova-ekaterina@yandex.ru>
2025-03-30 21:39:23 +0300

    Внесены корректировки в оформление кода

--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,27 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account_number
 from datetime import datetime
+
+from src.masks import get_mask_account_number, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
     """
-        Определяет тип входных данных (счет или карта) и маскирует номер.
+    Определяет тип входных данных (счет или карта) и маскирует номер.
 
-        Аргумент может быть, например:
-        - "Visa Platinum 7000792289606361"
-        - "Maestro 7000792289606361"
-        - "Счет 73654108430135874305"
+    Аргумент может быть, например:
+    - "Visa Platinum 7000792289606361"
+    - "Maestro 7000792289606361"
+    - "Счет 73654108430135874305"
 
-        Возвращает замаскированную строку, например:
-        - "Visa Platinum 7000 79** **** 6361" для карт
-        - "Счет **4305" для счетов
-        """
+    Возвращает замаскированную строку, например:
+    - "Visa Platinum 7000 79** **** 6361" для карт
+    - "Счет **4305" для счетов
+    """
     account_card_split = account_card.split()
     if "Счет" in account_card_split:
         return f"Счет {get_mask_account_number(account_card_split[1])}"
     else:
         card_numbers = []
-        card_name =[]
+        card_name = []
         for i in account_card_split:
             if i.isdigit():
                 card_numbers.append(i)
@@ -39,4 +39,3 @@ def get_date(date_str: str) -> str:
     """
     dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
     return dt.strftime("%d.%m.%Y")
-
