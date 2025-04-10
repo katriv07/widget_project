32c07819d42efba911cda4d2ca4646a0b2d1ead7
Югова Екатерина <yugova-ekaterina@yandex.ru>
2025-04-10 17:34:42 +0300

    Исправлена функция sort_by_date и файл readme

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict, List
+
+
 def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
     """Функция фильтрует операции в списке по ключу 'state'"""
     list_of_operation = []
@@ -7,7 +10,7 @@ def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
     return list_of_operation
 
 
-def sort_by_date(operations: list, flow: bool = True) -> list:
-    """Функция сортирует операции по дате"""
-    sort_by_date_list = sorted(operations, key=lambda item: item["date"], reverse=flow)
-    return sort_by_date_list
+def sort_by_date(operations: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Возвращает новый список, отсортированный по дате"""
+    return sorted(operations, key=lambda item: item["date"], reverse=True)
